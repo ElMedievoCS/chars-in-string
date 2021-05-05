fc86ca36fc5fc5853b268b89ac0b318ee03541dd
BGMP <jose@bgmp.cl>
2021-05-05 01:44:55 -0400

    Add instructions

--- src/cl/bgmp/Main.java
+++ src/cl/bgmp/Main.java
@@ -1,9 +1,33 @@
 package cl.bgmp;
 
+/**
+ * For your first assignment you must split the following {@link this#string} into characters, and print them all on screen, each
+ * into a different new line.
+ *
+ * The output of your program should look something like the following:
+ *
+ * >> S
+ * >> P
+ * >> L
+ * >> I
+ * >> T
+ * >>
+ * >> M
+ * >> E
+ * >>
+ * >> I
+ *
+ * etc...
+ *
+ * Happy coding!
+ *
+ */
 public class Main {
     private static final String string = "SPLIT ME INTO CHARACTERS";
 
     public static void main(String[] args) {
+
+        // Your code goes here.
 
     }
 }
